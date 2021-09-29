3ac82d725889086d4cb158595e215550aa8348ec
=DantaliakAFK <jairojosebandera@gmail.com>
2021-09-28 23:14:01 -0500

    Actu05

---
+++ APP NPC/backend/database.js
@@ -0,0 +1,7 @@
+/*module.exports = {
+    db: "mongodb://localhost:27017/productocaducado",
+  };*/
+
+  module.exports = {
+    db: "mongodb+srv://jairobandera:proyectonpc@cluster0.csokb.mongodb.net/ProyectoNPC?retryWrites=true&w=majority",
+  };
