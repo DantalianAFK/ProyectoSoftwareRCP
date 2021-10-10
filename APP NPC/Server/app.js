592f69fc0adb7ca8a19a59b3b49602cfbfbfb098
=DantaliakAFK <jairojosebandera@gmail.com>
2021-10-10 15:19:04 -0500

    Big Actu

---
+++ APP NPC/Server/app.js
@@ -0,0 +1,59 @@
+let express = require("express"),
+  cors = require("cors"),
+  mongoose = require("mongoose"),
+  database = require("./database"),
+  bodyParser = require("body-parser");
+
+// Connect mongoDB
+mongoose.Promise = global.Promise;
+mongoose
+  .connect("mongodb+srv://jairobandera:proyectonpc@cluster0.csokb.mongodb.net/ProyectoNPC?retryWrites=true&w=majority", 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      console.log("Database connected");
+    },
+    (error) => {
+      console.log("Database could't be connected to: " + error);
+    }
+  );
+
+const productAPI = require("../server/routes/product.route");
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(cors());
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+next();
+});
+// API
+app.use("/api", productAPI);
+
+// Create port
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+  console.log("Connected to port " + port);
+});
+
+// Find 404
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res) {
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
+});
