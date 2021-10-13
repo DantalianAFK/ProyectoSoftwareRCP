5f746e17eabd54c22bbae4da835ecffb9f45df7d
LuisRosero <rosero21318@gmail.com>
2021-10-12 22:26:03 -0500

    Aplicacion_Final

--- APP NPC/Server/database.js
+++
@@ -1,7 +0,0 @@
-/*module.exports = {
-    db: "mongodb://localhost:27017/productocaducado",
-  };*/
-
-  module.exports = {
-    db: "mongodb+srv://jairobandera:proyectonpc@cluster0.csokb.mongodb.net/ProyectoNPC?retryWrites=true&w=majority",
-  };
